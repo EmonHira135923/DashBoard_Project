43297d080772923042a51f2a9d757d7386e3b63d
EmonHira135923 <emonhossainhira231@gmail.com>
2025-05-29 21:21:57 +0600

    all data added

--- Django_Project/tasks/urls.py
+++ Django_Project/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import manager_dashboard,users_dashboard
+from tasks.views import manager_dashboard,users_dashboard,task_test
 
 urlpatterns = [
     path('user_dashboard/',users_dashboard),
-    path('manager_dashboard/',manager_dashboard)
+    path('manager_dashboard/',manager_dashboard),
+    path('task_test/',task_test)
 ]
--- Django_Project/tasks/views.py
+++ Django_Project/tasks/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def manager_dashboard(request):
-    return render(request,"manager_dashboard.html")
+    return render(request,"dashboard/manager_dashboard.html")
 
 def users_dashboard(request):
-    return render(request,"users_dashboard.html")
+    return render(request,"dashboard/users_dashboard.html")
     
 
+def task_test(request):
+    return render(request,"dashboard/test_static.html")
